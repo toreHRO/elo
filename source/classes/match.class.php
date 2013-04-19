afd4b29743b12c81d3b2c2ca0578f7901ba63162
torehro@googlemail.com <torehro@googlemail.com>
2013-04-19 21:59:26 +0200

    erste funktionen

--- source/classes/match.class.php
+++ source/classes/match.class.php
@@ -1,17 +1,110 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of match
  *
  * @author Tore
  */
 class match {
-    //put your code here
+    
+    private $id;
+    // Objekt der Klasse Team
+    private $home;
+    // Objekt der Klasse Team
+    private $away;
+    private $home_score;
+    private $away_score;
+	private $home_expected;
+	private $away_expected;
+    // ID: 0 = neutraler Platz, 1 = kein neutraler platz
+    private $venue;
+    // ID: 1 = Liga, 2 = Pokal, 3 = International; 4 = Freundschaftsspiel
+    private $comp;
+    
+    public function __construct( $_id, $_home = 0, $_away = 0, $_venue = 0, $_comp = 0 ) {
+		if ( $_id ) {
+			// Daten aus MySQL-DB holen
+		}
+		else {
+			$this->id = 0;
+			$this->home = $_home;
+			$this->away = $_away;
+			$this->venue = $_venue;
+			$this->comp = $_comp;
+		}
+    }
+    
+    public function get_id() {
+        return $this->id;
+    }
+	
+    public function set_id($id) {
+        $this->id = $id;
+    }
+
+    public function get_home() {
+        return $this->home;
+    }
+
+    public function set_home($home) {
+        $this->home = $home;
+    }
+
+    public function get_away() {
+        return $this->away;
+    }
+
+    public function set_away($away) {
+        $this->away = $away;
+    }
+
+    public function get_home_score() {
+        return $this->home_score;
+    }
+
+    public function set_home_score($home_score) {
+        $this->home_score = $home_score;
+    }
+
+    public function get_away_score() {
+        return $this->away_score;
+    }
+
+    public function set_away_score($away_score) {
+        $this->away_score = $away_score;
+    }
+
+    public function get_venue() {
+        return $this->venue;
+    }
+
+    public function set_venue($venue) {
+        $this->venue = $venue;
+    }
+
+    public function get_comp() {
+        return $this->comp;
+    }
+
+    public function set_comp($comp) {
+        $this->comp = $comp;
+    }
+
+	public function get_home_expected() {
+		return $this->home_expected;
+	}
+
+	public function set_home_expected($home_expected) {
+		$this->home_expected = $home_expected;
+	}
+
+	public function get_away_expected() {
+		return $this->away_expected;
+	}
+
+	public function set_away_expected($away_expected) {
+		$this->away_expected = $away_expected;
+	}    
 }
 
 ?>
--- source/classes/team.class.php
+++ source/classes/team.class.php
@@ -6,7 +6,57 @@
  * @author Tore
  */
 class team {
-    //put your code here
+    
+    private $id;
+    private $name;
+    // Object of rating
+    private $rating;
+    private $rating_points;
+    
+	public function __construct( $_id, $_rating_points = 0, $_name = "" ) {
+		if ( $_id ) {
+			// aus DB holen
+		}
+		else {
+			$this->id = 0;
+			$this->name = $_name;
+			$this->rating_points = $_rating_points;
+		}
+	}
+    
+    public function get_id() {
+		return $this->id;
+	}
+
+	public function set_id($id) {
+		$this->id = $id;
+	}
+
+	public function get_name() {
+		return $this->name;
+	}
+
+	public function set_name($name) {
+		$this->name = $name;
+	}
+
+	public function get_rating() {
+		return $this->rating;
+	}
+
+	public function set_rating($rating) {
+		$this->rating = $rating;
+	}
+
+	public function get_rating_points() {
+		return $this->rating_points;
+	}
+
+	public function set_rating_points($rating_points) {
+		$this->rating_points = $rating_points;
+	}
+
+
 }
 
 ?>
